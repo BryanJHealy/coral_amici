dccba0b487962b36d0e2c7d854248fd13da4fceb
Bryan Healy <32940823+BryanJHealy@users.noreply.github.com>
2022-12-30 23:13:48 -0500

    Progress Update

--- src/examples/generate_accompaniment.py
+++ src/examples/generate_accompaniment.py
@@ -1,14 +1,13 @@
 import pretty_midi
-from src.util import data_handling
+from src.util import data_handling as dh
 import argparse
 import sys
 import tensorflow as tf
-import numpy as np
 
 
 if __name__ == '__main__':
     # Command-line argument parsing for data path
-    parser = argparse.ArgumentParser(description='Transcribe Audio file to MIDI file')
+    parser = argparse.ArgumentParser(description='Generate accompaniment for given MIDI melody')
     parser.add_argument('model_path', action='store',
                         help='Path to the model')
     parser.add_argument('input', action='store',
@@ -19,29 +18,35 @@ if __name__ == '__main__':
                         help='create a piano roll png for the output generation')
     parameters = vars(parser.parse_args(sys.argv[1:]))
 
-    seed = 42
-    tf.random.set_seed(seed)
-    np.random.seed(seed)
-
     # Load model
     model = tf.keras.models.load_model(parameters['model_path'])
 
-    # Parse input midi file -> PrettyMidi
+    sequence_seconds = 15
+    start_at_seconds = 0
+    skip_empty_intro = True
+
+    # Parse input midi file using PrettyMidi and collect list of input sequence windows
     try:
         melody = pretty_midi.PrettyMIDI(parameters['input'])
+        melody_seq = dh.import_midi_sequence(parameters['input'], sequence_seconds, track='MELODY',
+                                             offset=start_at_seconds, skip_leading_space=skip_empty_intro)
     except:
         print('Unable to parse input file, exiting...')
         sys.exit(1)
 
-    generated_sequence = model(melody)
+    generated_seq = []
+    for input_window in melody_seq:
+        generated_seq.append(model.predict(input_window))
 
-    pm = data_handling.add_accompaniment_track(pm=melody,
-                                               accomp_notes=generated_sequence,
-                                               out_file='generated.mid',
-                                               initial_tempo=120,
-                                               melody_instrument=0,
-                                               accomp_instrument=32,
-                                               concat_sequential=True)
+    accompaniment_track = dh.build_accompaniment_track(generated_seq)
+
+    pm = dh.add_accompaniment_track(pm=melody,
+                                    accomp_notes=generated_seq,
+                                    out_file='generated.mid',
+                                    initial_tempo=120,
+                                    melody_instrument=0,
+                                    accomp_instrument=32,
+                                    concat_sequential=True)
 
     if parameters['roll']:
-        data_handling.plot_piano_roll(generated_sequence)
+        dh.plot_piano_roll(generated_seq)
--- src/util/data_handling.py
+++ src/util/data_handling.py
@@ -172,16 +172,16 @@ def plot_piano_roll(notes: pd.DataFrame, count: Optional[int] = None):
     plt.show()
 
 
-def add_accompaniment_track(pm: pretty_midi.PrettyMIDI, accomp_notes: pd.DataFrame, out_file: str,
+def build_accompaniment_track(windows, instrument_num=42,
                             velocity: int = 100,  # note loudness
-                            initial_tempo=80, melody_instrument=42,
-                            accomp_instrument=42, concat_sequential=True
-                            ) -> pretty_midi.PrettyMIDI:
-    acc_instrument = pretty_midi.Instrument(program=accomp_instrument, is_drum=False,
-                                            name='accompaniment')
+                            concat_sequential=True):
 
+    instrument = pretty_midi.Instrument(program=instrument_num, is_drum=False,
+                                            name='accompaniment')
     prev_start = 0
-    for i, note in accomp_notes.iterrows():
+    # for i, note in accomp_notes.iterrows():
+    sequence = []  # TODO: stack windows horizontally
+    for sample in sequence:
         start = float(prev_start + note['step'])
         end = float(start + note['duration'])
         note = pretty_midi.Note(
@@ -190,15 +190,81 @@ def add_accompaniment_track(pm: pretty_midi.PrettyMIDI, accomp_notes: pd.DataFra
             start=start,
             end=end,
         )
-        acc_instrument.notes.append(note)
+        instrument.notes.append(note)
         prev_start = start
+    return instrument
+
+
+def add_accompaniment_track(pm: pretty_midi.PrettyMIDI, accomp_windows, out_file: str,
+                            velocity: int = 100,  # note loudness
+                            instrument_num=42, concat_sequential=True
+                            ) -> pretty_midi.PrettyMIDI:
+
+    acc_instrument = build_accompaniment_track(accomp_windows, instrument_num, velocity=velocity,
+                              concat_sequential=concat_sequential)
 
     pm.instruments.append(acc_instrument)
     pm.write(out_file)
     return pm
 
-def import_midi_sequence(path, seconds, offset):
-    pm = pretty_midi.PrettyMIDI(path)
-    sequence = pretty_midi.PrettyMIDI()
 
-    return pm
+def midi_to_activation_sequence(song: pretty_midi.PrettyMIDI, seq_duration: float,
+                                instrument_track='MELODY',
+                                vocab_size=128, sample_frequency=60,
+                                offset=0, skip_leading_space=True):
+    pitch_col, start_col, end_col = 0, 1, 2
+
+    notes = None
+    for instrument_idx in range(len(song.instruments)):
+        if song.instruments[instrument_idx].name == instrument_track:
+            notes = midi_to_notes(song, instrument_index=instrument_idx)
+            notes = np.stack([notes[key] for key in key_order], axis=1)
+
+    def get_activation_tensor(note_events, sample_time, ds_idx):
+        activation = np.zeros(vocab_size)
+        for note_idx in range(ds_idx, len(note_events)):  # TODO check bounds
+            note = note_events[note_idx]
+            if note[start_col] <= sample_time:
+                if note[end_col] > sample_time:
+                    activation[int(note[pitch_col])] = 1.0  # TODO: vocab 0-based index?
+                else:
+                    ds_idx += 1  # increment index
+            else:
+                break
+        return activation, ds_idx
+
+    windows = []
+    mel_idx = 0
+    file_end_time = notes[-1][end_col]
+    file_samples = int(np.ceil(file_end_time * sample_frequency))
+    sequence_samples = seq_duration * sample_frequency
+
+    window = np.zeros((vocab_size, sequence_samples))
+    for sample_num in range(file_samples):
+        sample_time = sample_num / sample_frequency
+        active_mel_pitches, mel_idx = get_activation_tensor(notes, sample_time, mel_idx)
+
+        if sample_num < sequence_samples:  # build entire first window
+            window[:, sample_num] = active_mel_pitches
+        else:  # shift windows by one sample
+            window = window[:, 1:]
+            window = np.append(window, active_mel_pitches)
+
+        windows.append(window)
+
+    return windows
+
+
+def import_midi_sequence(path, seconds, track='MELODY', offset=0, skip_leading_space=True):
+    song = pretty_midi.PrettyMIDI(path)
+
+    song_windows = midi_to_activation_sequence(song, seconds, instrument_track='MELODY',
+                                               vocab_size=128, sample_frequency=60,
+                                               offset=offset, skip_leading_space=skip_leading_space)
+
+    for instrument_idx in range(len(song.instruments)):
+        if song.instruments[instrument_idx].name == track:
+            melody_notes = midi_to_notes(song, instrument_index=instrument_idx)
+            melody_notes = np.stack([melody_notes[key] for key in key_order], axis=1)
+
+    return song_windows
